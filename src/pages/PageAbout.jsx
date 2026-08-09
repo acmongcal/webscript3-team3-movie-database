3f1a16b788addb1e6e1a8855e74b31856e4f6cc6
Johnny Zhu <jz95zhu@gmail.com>
2026-08-08 22:18:27 -0700

    put header hamburger menu, responsiveness of footer

--- src/pages/PageAbout.jsx
+++ src/pages/PageAbout.jsx
@@ -1,7 +1,30 @@
 function PageAbout() {
   return (
     <section>
-      <h2>About</h2>
+      <h2>About Animovies</h2>
+      <article>
+        <p>
+          Welcome to Animovies, an anime movie database created for fans who
+          want an easy way to discover and explore animated films from Japan.
+          Our platform brings together information on popular classics, modern
+          releases, and hidden gems, making it simple to find your next movie to
+          watch.
+        </p>
+        <p>
+          At Animovies, you can browse detailed movie pages featuring story
+          summaries, genres, release dates, studios, runtimes, ratings, and
+          trailers. Whether you're a longtime anime fan or just beginning your
+          journey, our goal is to provide accurate information through a clean
+          and easy-to-use experience.
+        </p>
+        <p>
+          {" "}
+          We believe every anime movie has a story worth discovering. That's why
+          we're committed to building a reliable database where fans can explore
+          new releases, revisit timeless classics, and learn more about the
+          films and creators behind them.
+        </p>
+      </article>
     </section>
   );
 }
