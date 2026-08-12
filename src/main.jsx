00867ed0919618d53cfb4bde280a8442cf566dcb
Johnny Zhu <jz95zhu@gmail.com>
2026-08-12 13:49:27 -0700

    react bootstrap setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import AppRouter from './routers/AppRouter.jsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import AppRouter from "./routers/AppRouter.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/styles.scss";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AppRouter />
   </StrictMode>,
-)
+);
