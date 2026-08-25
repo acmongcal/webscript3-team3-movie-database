a95f6af36d7ab17333c80da99842f09b9ac32a44
Alan Christopher Mongcal <aranmongcal@gmail.com>
2026-08-25 10:00:13 -0700

    added place holder for banner

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,6 +1,7 @@
 import { Carousel, Image } from "react-bootstrap";
 import { useState} from "react";
 import { IMAGE_BASE_URL} from "../global/globals";
+import posterPlaceholder from "../assets/images/poster-placeholder.svg";
 
 function HeroBanner({ movies }) {
   const [index, setIndex] = useState(0);
@@ -16,7 +17,7 @@ function HeroBanner({ movies }) {
         <Carousel.Item interval={4000} key={i}>
           <Image
             className="banner-cover"
-            src={IMAGE_BASE_URL + movie.backdrop_path}
+            src={movie.backdrop_path? IMAGE_BASE_URL + movie.backdrop_path:posterPlaceholder}
             alt={movie.title}
           />
           <Carousel.Caption>
